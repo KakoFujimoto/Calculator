a48572a4a6ad041f340d164b79ae95126363bedb
藤本 華子  K.F. <k.fujimoto@datakit.local>
2025-03-26 06:50:51 +0900

    EqualCommandのインスタンスメソッドに関する修正

--- Caluclator/src/commands/op/equal-command.ts
+++ Caluclator/src/commands/op/equal-command.ts
@@ -2,22 +2,20 @@ import { Command } from "../base/command";
 import { Calculator } from "../../calculator/context";
 
 export class EqualCommand implements Command {
-    private calculator = Calculator;
+    private calculator: Calculator; // Calculatorクラスのインスタンスを保持
 
-    constructor(calculator:Calculator){
-       this.calculator = calculator;
-
+    constructor(calculator: Calculator) {
+        this.calculator = calculator;
     }
 
     execute(): void {
         try {
-
             const currentExpression = this.calculator.getCurrentValue();
             if (!currentExpression) return;
 
             const result = eval(currentExpression);
             this.calculator.setValue(result.toString());
-        } catch(error) {
+        } catch (error) {
             this.calculator.setValue("Error");
         }
     }
